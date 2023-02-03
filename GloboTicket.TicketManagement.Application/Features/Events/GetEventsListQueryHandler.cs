708609225bf684941408c5f8059851653b3f3d08
carlosld <cleondorta@gmail.com>
2023-02-03 21:50:24 +0000

    Add project files.

---
+++ GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events;
+
+public class GetEventsListQueryHandler : IRequestHandler<GetEventsListQuery, List<EventListVm>>
+{
+    private readonly IMapper mapper;
+    private readonly IAsyncRepository<Event> eventRepository;
+
+    public GetEventsListQueryHandler(IMapper mapper, IAsyncRepository<Event> eventRepository)
+    {
+        this.mapper = mapper;
+        this.eventRepository = eventRepository;
+    }
+
+    public async Task<List<EventListVm>> Handle(GetEventsListQuery request, CancellationToken cancellationToken)
+    {
+        var allEvents = (await eventRepository.ListAllAsync())
+            .OrderBy(x => x.Date);
+        return mapper.Map<List<EventListVm>>(allEvents);
+    }
+}
